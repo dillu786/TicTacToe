6dc8cbe7fdcb779579460769c6c449588d5438e5
dillu786 <96700236+dillu786@users.noreply.github.com>
2023-11-12 20:11:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -641,7 +641,7 @@ const onReset=()=>{
    if(singlePlayer)
    {
       let sudoku=Array(9).fill(null);
-      let position=allPositions[Math.floor(Math.random( )* 9)];
+      let position=corners[Math.floor(Math.random( )* 9)];
       const squaresCopy=[...sudoku];
       squaresCopy[position]="X";
       
